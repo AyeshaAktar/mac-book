bb6c55780e8d5558673945dc9461d2059da5b2f5
AyeshaAktar <ayeshaaktar491@gmail.com>
2021-08-21 22:07:29 +0600

    apply

--- js/mac-book.js
+++ js/mac-book.js
@@ -9,7 +9,9 @@ const initialMemoryCost = document.getElementById('memory-price');
 const initialStorageCost = document.getElementById('storage-price');
 const initialDeliveryCharge = document.getElementById('delivery-charge');
 const total = document.getElementById('total');
-const discuntTotal = document.getElementById('grand-total');
+const grandTotal = document.getElementById('grand-total');
+const applyBtn = document.getElementById('apply');
+const promoCode = document.getElementById('promo-code')
 
 
 function macBook(price, initialCost){
@@ -20,10 +22,6 @@ function macBook(price, initialCost){
 //memory
 memory8Gb.addEventListener('click', function(){
     macBook(0, initialMemoryCost);
-   
-    // const memoryCost = 0;
-    // initialMemoryCost.innerText =memoryCost;
-    //     totalPrice();
 })
 memory16Gb.addEventListener('click', function(){
     macBook(180, initialMemoryCost);
@@ -33,35 +31,20 @@ memory16Gb.addEventListener('click', function(){
 //storage
 storage256Gb.addEventListener('click', function(){
     macBook(0, initialStorageCost);
-    // const storageCost = 0;
-    // initialStorageCost.innerText =storageCost;
-    // totalPrice();
 })
 storage512Gb.addEventListener('click', function(){
     macBook(100, initialStorageCost);
-    // const storageCost = 100;
-    // initialStorageCost.innerText = storageCost;
-    //    totalPrice();
 })
 storage1Tb.addEventListener('click', function(){
     macBook(180, initialStorageCost);
-    // const storageCost = 180;
-    //     initialStorageCost.innerText = storageCost;
-    //     totalPrice();
 })
 
 //delivery cost
 deliveryFree.addEventListener('click', function(){
     macBook(0, initialDeliveryCharge);
-    // const deliveryCharge = 0;
-    // initialDeliveryCharge.innerText = deliveryCharge;
-    // totalPrice();
 })
 deliveryPaid.addEventListener('click', function(){
     macBook(20, initialDeliveryCharge);
-    // const deliveryCharge = 20;
-    //     initialDeliveryCharge.innerText = deliveryCharge;
-    //     totalPrice();
 })
 
 function totalPrice(){
@@ -72,18 +55,19 @@ function totalPrice(){
     const totalPrice = bestPrice + parseInt(extraMemoryCost) + parseInt(extraStorageCost) + parseInt(deliveryCharge);
     total.innerText = totalPrice;
 
-    discuntTotal.innerText = totalPrice;
+    grandTotal.innerText = totalPrice;
 }
 
 
-document.getElementById('apply').addEventListener('click', function(){
-    const codeField = document.getElementById('promo-code');
-    // console.log('clicked');
-    const promoCode = codeField.value;
-    const discunt = 20;
-    if(promoCode == 'stevekaku'){ 
-    const discuntPrice = discuntTotal * discunt / 100;
-        discuntTotal.innerText = discuntPrice;
+applyBtn.addEventListener('click',function(){
+    const promoText = promoCode.value;
+    const withoutDiscount = total.innerText;
+    if(promoText == 'stevekaku'){
+        const discountPrice = parseInt(withoutDiscount)*0.20;
+        const totalDiscountPrice = parseInt(withoutDiscount) - discountPrice;
+        grandTotal.innerText = totalDiscountPrice;
     }
-    codeField.value = '';
+    else{
+        alert('Wrong promo code! Please try again')
+    }
 })
