031e12f73e186efc931c8b33f9bd788857d77651
AyeshaAktar <ayeshaaktar491@gmail.com>
2021-08-21 16:29:57 +0600

    add function

--- js/mac-book.js
+++ js/mac-book.js
@@ -13,60 +13,54 @@ const grandTotal = document.getElementById('grand-total');
 
 //memory
 memory8Gb.addEventListener('click', function(){
-    const memoryCost = 0;
-    initialMemoryCost.innerText =memoryCost;
-    totalPrice();
+    macBook(0, initialMemoryCost);
+    // const memoryCost = 0;
+    // initialMemoryCost.innerText =memoryCost;
+    //     totalPrice();
 })
 memory16Gb.addEventListener('click', function(){
-    const memoryCost = 180;
-    const memorCostValue= initialMemoryCost.innerText;
-    const totalMemoryCost = parseInt(memorCostValue) + memoryCost;
-    if(totalMemoryCost == 180){
-        initialMemoryCost.innerText = totalMemoryCost;
-        totalPrice();
-    }
+    macBook(180, initialMemoryCost);
 })
 
 //storage
 storage256Gb.addEventListener('click', function(){
-    const storageCost = 0;
-    initialStorageCost.innerText =storageCost;
-    totalPrice();
+    macBook(0, initialStorageCost);
+    // const storageCost = 0;
+    // initialStorageCost.innerText =storageCost;
+    // totalPrice();
 })
 storage512Gb.addEventListener('click', function(){
-    const storageCost = 100;
-    const storageCostValue= 0;
-    const totalStorageCost = parseInt(storageCostValue) + storageCost;
-    if(totalStorageCost == 100){
-        initialStorageCost.innerText = totalStorageCost;
-        totalPrice();
-    }
+    macBook(100, initialStorageCost);
+    // const storageCost = 100;
+    // initialStorageCost.innerText = storageCost;
+    //    totalPrice();
 })
 storage1Tb.addEventListener('click', function(){
-    const storage1TbCost = 180;
-    const storage1TbCostValue= 0;
-    const totalStorage1TbCost = parseInt(storage1TbCostValue) + storage1TbCost;
-    if(totalStorage1TbCost == 180){
-        initialStorageCost.innerText = totalStorage1TbCost;
-        totalPrice();
-    }
+    macBook(180, initialStorageCost);
+    // const storageCost = 180;
+    //     initialStorageCost.innerText = storageCost;
+    //     totalPrice();
 })
 
 //delivery cost
 deliveryFree.addEventListener('click', function(){
-    const deliveryCharge = 0;
-    initialDeliveryCharge.innerText = deliveryCharge;
-    totalPrice();
+    macBook(0, initialDeliveryCharge);
+    // const deliveryCharge = 0;
+    // initialDeliveryCharge.innerText = deliveryCharge;
+    // totalPrice();
 })
 deliveryPaid.addEventListener('click', function(){
-    const deliveryCharge = 20;
-    const deliveryChargeValue= initialDeliveryCharge.innerText;
-    const totaldeliveryCost = parseInt(deliveryChargeValue) + deliveryCharge;
-    if(totaldeliveryCost == 20){
-        initialDeliveryCharge.innerText = totaldeliveryCost;
-        totalPrice();
-    }
+    macBook(20, initialDeliveryCharge);
+    // const deliveryCharge = 20;
+    //     initialDeliveryCharge.innerText = deliveryCharge;
+    //     totalPrice();
 })
+
+function macBook(price, initialCost){
+    const initialPrice = price;
+    initialCost.innerText = initialPrice;
+    totalPrice();
+}
 
 function totalPrice(){
     const bestPrice = 1299;
@@ -76,6 +70,29 @@ function totalPrice(){
     const totalPrice = bestPrice + parseInt(extraMemoryCost) + parseInt(extraStorageCost) + parseInt(deliveryCharge);
     total.innerText = totalPrice;
 }
+
+
+
+
+
+
+
+
+
+/* function memory(price){
+    // const memoryCost8Gb = 0;
+    // const memoryCost16Gb = 180;
+    const memorCostValue= initialMemoryCost.innerText;
+    const totalMemoryCost = parseInt(memorCostValue) + price;
+    if(totalMemoryCost == 180){
+        initialMemoryCost.innerText = totalMemoryCost;
+        // totalPrice();
+    }
+    else {
+        initialMemoryCost.innerText =totalMemoryCost;
+    }
+    totalPrice();
+} */
 
 
 
